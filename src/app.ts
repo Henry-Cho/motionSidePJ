e072f17ff5106401f10ec00f7e3254d6c76d6b8d
Hyungseok Cho <hyungseokcho@Hyungseoks-MacBook-Pro.local>
2021-07-09 00:55:33 +0900

    insert image & page components

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import { ImageComponent } from "./components/page/item/image.js";
+import { PageComponent } from "./components/page/page.js";
+
+class App {
+    private readonly page: PageComponent;
+    // private readonly image: ImageComponent;
+    constructor(appRoot: HTMLElement) {
+        this.page = new PageComponent();
+        this.page.attachTo(appRoot);
+        const image = new ImageComponent("ImageTitle", `https://picsum.photos/600/300`);
+    
+        image.attachTo(appRoot, 'beforeend')
+    }
+}
+
+new App(document.querySelector('.main_inner') ! as HTMLElement);
